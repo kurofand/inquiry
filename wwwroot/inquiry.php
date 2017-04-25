f21fe879dd873b2b7146f683c624caa199cff9d2
Loginov Vladimir <elfandx_0209@yahoo.co.jp>
2017-04-25 18:44:26 +0900

    2017 4 25 commit

---
+++ wwwroot/inquiry.php
@@ -0,0 +1,12 @@
+<?php ?>
+<html>
+<body>
+<form action="./inquiry_fin.php" method="post">
+email(*):<input type="text" name="email"><br>
+名前<input type="text" name="name"><br>
+誕生日<input type="text" name="birthday"><br>
+問い合わせ内容<textarea name="body"></textarea><br>
+<button>問い合わせ</button>
+</form>
+<body>
+</html>
---
+++ wwwroot/inquiry_fin.php
@@ -0,0 +1,26 @@
+<?php
+/*$email=(string)@$_POST['email'];
+$email=(string)filter_input(INPUT_POST. 'email');*/
+$params=array('email', 'name', 'birthday', 'body');
+$input_data=array();
+foreach($params as $p)
+	$input_data[$p]=(string)@$_POST[$p];
+var_dump($input_data);
+$error_detail=array();
+$must_params=array('email', 'body');
+foreach($must_params as $p)
+	if(''===$input_data[$p])
+		$error_detail["error_must_[$p]"]=true;
+if(false===filter_var($input_data['email']. FILTER_VALIDATE_EMAIL))
+	$error_detail["error_format_email"]=true;
+if(''!==$input_data['birthday']);
+if(false==strtotime($input_data['birthday']))
+	$error_detail["error_format_birthday"]=true;
+
+
+if(array()!==$error_detail)
+{
+	echo 'エラーがあったらしい';
+	exit;
+}
+echo 'でーたのvalidateはOKでした';
